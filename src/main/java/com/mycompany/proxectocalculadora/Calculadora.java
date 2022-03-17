dde71979d0ee74a1d5622e7ded74a3586ede65a3
Alejandro García <a21alejandrogl@iessanclemente.net>
2022-03-17 13:12:17 +0100

    Clase calculadora do temario 4 de Contornos de Desenvolvemento

---
+++ src/main/java/com/mycompany/proxectocalculadora/Calculadora.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proxectocalculadora;
+
+/**
+ *
+ * @author Alex
+ */
+public class Calculadora {
+    
+    private int num1;
+    public int suma;
+    public int resta;
+    public int multiplicacion;
+    public int division;
+    public int getNum1() {
+        return num1;
+    }
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+    private int num2;
+    public int getNum2() {
+        return num2;
+    }
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+    
+    public Calculadora() {
+    }
+    
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public void suma() {
+        int suma = num1 + num2;
+    }
+    public void resta() {
+        int resta = num1 - num2;
+    }    
+    public void multiplicacion() {
+        int multiplicacion = num1 * num2;
+    }
+    public void division() {
+        int division = num1 / num2;
+    }    
+}
