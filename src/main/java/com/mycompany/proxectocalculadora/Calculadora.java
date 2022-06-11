5f9940a10c0bd4534821edd6723dc509b3185307
Alejandro Garci­a <a21alejandrogl@iessanclemente.net>
2022-06-11 10:34:17 +0200

    Clase calculadora do temario 4 de Contornos de Desenvolvemento

--- src/main/java/com/mycompany/proxectocalculadora/Calculadora.java
+++ src/main/java/com/mycompany/proxectocalculadora/Calculadora.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.proxectocalculadora;
 
+// asd
+        
 /**
  *
- * @author Alex
+ * @author Alex García Losas
  */
 public class Calculadora {
     
